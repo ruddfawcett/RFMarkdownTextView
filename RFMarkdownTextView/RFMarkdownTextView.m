2424621773be72e014b411b1d0ff23464ad79d1b
Mark Hudnall <hudnall.mark@gmail.com>
2015-06-14 01:06:23 -0700

    * Wrap selected text with inserted symbol * Newlines will pick up bullets or checkboxes

--- RFMarkdownTextView/RFMarkdownTextView.m
+++ RFMarkdownTextView/RFMarkdownTextView.m
@@ -72,20 +72,51 @@
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
+- (void)wrapSelectedRangeWithString:(NSString *)string {
+    [self wrapSelectedRangeWithStartString:string endString:string];
+}
+
+- (void)wrapSelectedRangeWithStartString:(NSString *)startString endString:(NSString *)endString {
+    NSUInteger length = self.selectedRange.length;
+    NSUInteger location = self.selectedRange.location;
+    [self setSelectionRange:NSMakeRange(self.selectedRange.location, 0)];
+    [self insertText:startString];
+    NSUInteger endLocation = self.selectedRange.location + length;
+    [self setSelectionRange:NSMakeRange(endLocation, 0)];
+    [self insertText:endString];
+    [self setSelectionRange:NSMakeRange(location + startString.length, length)];
+}
+
 - (NSArray *)buttons {
     return @[
              [self createButtonWithTitle:@"#" andEventHandler:^{ [self insertText:@"#"]; }],
              [self createButtonWithTitle:@"*" andEventHandler:^{
-                 [self insertText:@"*"];
+                 if (self.selectedRange.length > 0) {
+                     [self wrapSelectedRangeWithString:@"*"];
+                 } else {
+                     [self insertText:@"*"];
+                 }
              }],
              [self createButtonWithTitle:@"->|" andEventHandler:^{ [self insertText:@"  "]; }],
              [self createButtonWithTitle:@"[[" andEventHandler:^{
-                 NSRange selectionRange = self.selectedRange;
-                 selectionRange.location += 2;
-                 [self insertText:@"[[]]"];
-                 [self setSelectionRange:selectionRange];
+                 if (self.selectedRange.length > 0) {
+                     [self wrapSelectedRangeWithStartString:@"[[" endString:@"]]"];
+                     NSString *linkName = [self textInRange:self.selectedTextRange];
+                     [self replaceRange:self.selectedTextRange withText:[linkName capitalizedString]];
+                 } else {
+                     NSRange selectionRange = self.selectedRange;
+                     selectionRange.location += 2;
+                     [self insertText:@"[[]]"];
+                     [self setSelectionRange:selectionRange];
+                 }
              }],
-             [self createButtonWithTitle:@"`" andEventHandler:^{ [self insertText:@"`"]; }],
+             [self createButtonWithTitle:@"`" andEventHandler:^{
+                 if (self.selectedRange.length > 0) {
+                     [self wrapSelectedRangeWithString:@"`"];
+                 } else {
+                     [self insertText:@"`"];
+                 }
+             }],
              [self createButtonWithTitle:@"Photo" andEventHandler:^{
                  ImageBlock block = ^(NSString *imagePath) {
                      NSRange selectionRange = self.selectedRange;
@@ -121,6 +152,28 @@
 
 - (RFToolbarButton *)createButtonWithTitle:(NSString*)title andEventHandler:(void(^)())handler {
     return [RFToolbarButton buttonWithTitle:title andEventHandler:handler forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
+    if ([text isEqualToString:@"\n"]) {
+        NSString *pattern = @" *(\\*|- \\[( |x)\\]) ";
+        NSError  *error = nil;
+        NSRegularExpression* regex = [NSRegularExpression regularExpressionWithPattern: pattern options:0 error:&error];
+        
+        if (range.location > 0) {
+            NSRange oldRange = NSMakeRange(MAX(0, range.location - 1), range.length);
+            NSRange previousLineRange = [self.text lineRangeForRange:oldRange];
+            
+            NSArray *matches = [regex matchesInString:self.text options:0 range: previousLineRange];
+            
+            if (matches.count > 0) {
+                NSString *previousPrefix = [self.text substringWithRange:[matches[0] rangeAtIndex:0]];
+                [self insertText:[NSString stringWithFormat:@"\n%@", previousPrefix]];
+                return NO;
+            }
+        }
+    }
+    return YES;
 }
 
 - (void)textViewDidChange:(UITextView *)textView {
